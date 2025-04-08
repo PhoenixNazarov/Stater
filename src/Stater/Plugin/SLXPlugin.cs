d752ce40693dd37d18ae5b23c17462344c719b16
phoenix <vova24848@gmail.com>
2025-04-08 09:00:46 +0300

    SF-20: add project tests workflow

--- src/Stater/Plugin/SLXPlugin.cs
+++ src/Stater/Plugin/SLXPlugin.cs
@@ -6,11 +6,6 @@ public class SLXPlugin: ButtonFilePlugin
 {
     public override PluginOutput Start(PluginInput pluginInput, string file)
     {
-        // if (dialogResult != DialogResult.OK)
-        // {
-        // result = PluginOutput.From("DialogResult is not OK");
-        // return result;
-        // }
         var parser = new Parser(file);
         var stateflow = parser.Parse();
         var pluginStateflow = new Translator().Convert(stateflow);
